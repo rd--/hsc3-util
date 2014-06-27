dc7ba5d6cb0c35522e6405a415be60d792c547d9
Rohan Drape <rd@slavepianos.org>
2014-06-27 09:11:59 +0000

    ~hs/scsyndef-to-dot(~hsc3)

--- hs/scsyndef-to-dot.hs
+++ hs/scsyndef-to-dot.hs
@@ -3,8 +3,8 @@ import System.Environment {- base -}
 import System.FilePath {- filepath -}
 import System.Process {- process -}
 
-import Sound.SC3 {- hsc3 -}
-import qualified Sound.SC3.Server.Synthdef.Read as R
+import qualified Sound.SC3.Server.Graphdef as R {- hsc3 -}
+import qualified Sound.SC3.Server.Graphdef.Read as R {- hsc3 -}
 
 import Sound.SC3.UGen.Dot {- hsc3-dot -}
 import qualified Sound.SC3.UGen.Dot.Internal as D
@@ -14,8 +14,8 @@ scsyndef_to_dot opt sy_nm = do
   gr <- R.read_graphdef_file sy_nm
   let dot_nm = replaceExtension sy_nm "dot"
       svg_nm = replaceExtension sy_nm "svg"
-      sy = R.graphdef_to_synthdef gr
-      dr = D.dotGraph svg_options (synthdefGraph sy)
+      (_,gr') = R.graphdef_to_graph gr
+      dr = D.dotGraph svg_options gr'
   writeFile dot_nm dr
   when opt (void (rawSystem "dot" ["-T","svg",dot_nm,"-o",svg_nm]))
   return ()
