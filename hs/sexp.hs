252d5c6c8fb170c57861519d74169802fd5cfd76
Rohan Drape <rd@slavepianos.org>
2014-10-23 03:59:58 +0000

    +hs/sexp

---
+++ hs/sexp.hs
@@ -0,0 +1,9 @@
+import Sound.SC3.RW.SExp {- hsc3-rw -}
+import System.Environment {- base -}
+
+main :: IO ()
+main = do
+  a <- getArgs
+  case a of
+    [i_fn,o_fn] -> hs_to_lisp_io i_fn o_fn
+    _ -> error "input-file output-file"
