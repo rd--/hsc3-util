e657b80e7c15d2c4b4709f9b54a8cca3f8626e56
Rohan Drape <rd@slavepianos.org>
2015-04-17 01:51:13 +0000

    ~->dot(+-h)

--- hs/scsyndef-to-dot.hs
+++ hs/scsyndef-to-dot.hs
@@ -6,7 +6,7 @@ import System.Process {- process -}
 import qualified Sound.SC3.Server.Graphdef as R {- hsc3 -}
 import qualified Sound.SC3.Server.Graphdef.Read as R {- hsc3 -}
 
-import Sound.SC3.UGen.Dot {- hsc3-dot -}
+import qualified Sound.SC3.UGen.Dot as D {- hsc3-dot -}
 import qualified Sound.SC3.UGen.Dot.Internal as D {- hsc3-dot -}
 
 scsyndef_to_dot :: Bool -> FilePath -> IO ()
@@ -15,15 +15,17 @@ scsyndef_to_dot opt sy_nm = do
   let dot_nm = replaceExtension sy_nm "dot"
       svg_nm = replaceExtension sy_nm "svg"
       (_,gr') = R.graphdef_to_graph gr
-      dr = D.dotGraph (if opt then svg_options else dot_options) gr'
+      dr = D.dotGraph (if opt then D.svg_options else D.dot_options) gr'
   writeFile dot_nm dr
   when opt (void (rawSystem "dot" ["-T","svg",dot_nm,"-o",svg_nm]))
   return ()
 
 main :: IO ()
 main = do
+  let usage = putStrLn "scsyndef-to-dot [-svg] file..."
   arg <- getArgs
   case arg of
-    [] -> putStrLn "scsyndef-to-dot [-svg] file..."
+    [] -> usage
+    ["-h"] -> usage
     "-svg":nm -> mapM_ (scsyndef_to_dot True) nm
     nm -> mapM_ (scsyndef_to_dot False) nm
