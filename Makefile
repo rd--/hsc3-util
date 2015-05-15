112b87dd1cf82cdb9ab4b4dfda47749c49dad932
Rohan Drape <rd@slavepianos.org>
2015-05-15 05:36:22 +0000

    ~hsc3-setup

--- Makefile
+++ Makefile
@@ -4,3 +4,6 @@ clean:
 
 push-sp:
 	darcs push -a rd@slavepianos.org:sw/hsc3-utils
+
+build-setup:
+	(cd hs; make build-setup)
--- hs/setup.hs
+++ hs/setup.hs
@@ -1,3 +1,4 @@
+import Control.Monad {- base -}
 import System.Directory {- directory -}
 import System.Environment {- base -}
 import System.Exit {- base -}
@@ -19,12 +20,12 @@ plain =
 ext :: [String]
 ext =
     ["after-pim"
+    ,"hmeap","hmeap-utils"
     ,"hosc-json","hosc-utils"
     ,"hsc3-auditor","hsc3-cairo","hsc3-data","hsc3-rec","hsc3-rw","hsc3-unsafe","hsc3-utils"
-    ,"hsdif"]
+    ,"hsdif","hsharc","hspear"]
 
-put_l,put_w :: [String] -> IO ()
-put_l = putStrLn . unlines
+put_w :: [String] -> IO ()
 put_w = putStrLn . unwords
 
 pkg_set :: String -> [String]
@@ -32,6 +33,7 @@ pkg_set nm =
     case nm of
       "core" -> core
       "plain" -> plain
+      "core+plain" -> core ++ plain
       "ext" -> ext
       "all" -> concat [core,plain,ext]
       _ -> error "hsc3-setup: unknown pkg_set"
@@ -57,17 +59,33 @@ s_update nm src dst = do
   let f pkg = s_cwd (dst </> pkg) >> s_run "darcs" ["pull",src </> pkg]
   mapM_ f (pkg_set nm)
 
-s_at_each :: String -> FilePath -> String -> [String] -> IO ()
-s_at_each nm dir cmd args = do
-  let f pkg = s_cwd (dir </> pkg) >> s_run cmd args
+s_at_each :: String -> Maybe FilePath -> (String -> (String,[String])) -> IO ()
+s_at_each nm dir gen = do
+  let f pkg = let (cmd,args) = gen pkg
+                  act = s_run cmd args
+              in case dir of
+                   Nothing -> act
+                   Just dir' -> s_cwd (dir' </> pkg) >> act
   mapM_ f (pkg_set nm)
+
+s_at_each' :: String -> Maybe FilePath -> String -> [String] -> IO ()
+s_at_each' nm dir cmd args = s_at_each nm dir (const (cmd,args))
+
+s_with_all :: String -> FilePath -> ([FilePath] -> (String, [String])) -> IO ()
+s_with_all nm dir gen =
+    let pkg = pkg_set nm
+        pkg' = map (dir </>) pkg
+        (cmd,args) = gen pkg'
+    in void (s_run cmd args)
 
 main :: IO ()
 main = do
   a <- getArgs
   case a of
-    ["echo",nm] -> s_echo nm
     ["clone",nm,src,dst] -> s_clone nm src dst
+    ["echo",nm] -> s_echo nm
+    "local":nm:dir:cmd:arg -> s_at_each' nm (Just dir) cmd arg
+    ["rebuild",nm,dir] -> s_with_all nm dir (\pkg -> ("cabal","install" : pkg))
+    ["unregister",nm] -> s_at_each nm Nothing (\pkg -> ("ghc-pkg",["unregister","--force",pkg]))
     ["update",nm,src,dst] -> s_update nm src dst
-    ["clean",nm,dir] -> s_at_each nm dir "cabal" ["clean"]
-    _ -> putStrLn "hsc3-setup"
+    _ -> putStrLn "hsc3-setup {clone|echo|local|unregister|update}"
